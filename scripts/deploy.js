27351206390dac8210c7f4387a1c73a575113ded
michaelcohen716 <michaelcohen716@gmail.com>
2020-11-25 07:48:37 -0800

    add audit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,35 +1,35 @@
 const ADDRESSES = {
 	aave: {
 		kovan: '0xb597cd8d3217ea6477232f9217fa70837ff667af',
-        mainnet: '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9'
-    },
-    votingAave: {
-        kovan: '0x690eAcA024935Aaff9B14b9FF9e9C8757a281f3C',
-        mainnet: '0x0671CA7E039af2cF2D2c5e7F1Aa261Ae78B3ffDF'
-    },
+		mainnet: '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9',
+	},
+	votingAave: {
+		kovan: '0x690eAcA024935Aaff9B14b9FF9e9C8757a281f3C',
+		mainnet: '0x0671CA7E039af2cF2D2c5e7F1Aa261Ae78B3ffDF',
+	},
 	stakedAave: {
-        kovan: '0xf2fbf9A6710AfDa1c4AaB2E922DE9D69E0C97fd2',
-        mainnet: '0x4da27a545c0c5B758a6BA100e3a049001de870f5'
+		kovan: '0xf2fbf9A6710AfDa1c4AaB2E922DE9D69E0C97fd2',
+		mainnet: '0x4da27a545c0c5B758a6BA100e3a049001de870f5',
 	},
 	governance: {
-        kovan: '0x374d0940dc9a980219e0aA6566C3067159d2F442',
-        mainnet: '0x8a2Efd9A790199F4c94c6effE210fce0B4724f52'
+		kovan: '0x374d0940dc9a980219e0aA6566C3067159d2F442',
+		mainnet: '0x8a2Efd9A790199F4c94c6effE210fce0B4724f52',
 	},
 	kyberProxy: {
 		kovan: '0x692f391bCc85cefCe8C237C01e1f636BbD70EA4D',
-        mainnet: '0x818E6FECD516Ecc3849DAf6845e3EC868087B755'
-    },
-    proxyAdmin: {
-        kovan: '0x803428e38DBFDf2EB25D94B538A1CFc395E66615',
-        mainnet: ''
-    },
+		mainnet: '0x818E6FECD516Ecc3849DAf6845e3EC868087B755',
+	},
+	proxyAdmin: {
+		kovan: '0x803428e38DBFDf2EB25D94B538A1CFc395E66615',
+		mainnet: '',
+	},
 	cosigner1: {
 		kovan: '0x885583955F14970CbC0046B91297e9915f4DE6E4',
-        mainnet: ''
+		mainnet: '',
 	},
 	cosigner2: {
 		kovan: '0x5314736b4b7778aC25be9afb3819c4ABF4FBEaEA',
-        mainnet: ''
+		mainnet: '',
 	},
 };
 
@@ -69,13 +69,13 @@ async function main() {
 		'500',
 		'500',
 		'100',
-        'xAAVEa',
-        'Samuelson'
+		'xAAVEa',
+		'Samuelson'
 	);
 
-    await xaaveProxyCast.approveStakingContract();
-    await xaaveProxyCast.approveKyberContract(ADDRESSES['aave'][network]);
-    console.log('xaaveProxy', xaaveProxyCast.address);
+	await xaaveProxyCast.approveStakingContract();
+	await xaaveProxyCast.approveKyberContract(ADDRESSES['aave'][network]);
+	console.log('xaaveProxy', xaaveProxyCast.address);
 }
 
 main()
@@ -86,5 +86,5 @@ main()
 	});
 
 module.exports = {
-    ADDRESSES
-}
+	ADDRESSES,
+};
